05c8086784d14144dbc3aef2c1400784d5b9c905
nikodps58 <nicolasdelpozo58@gmail.com>
2024-01-11 20:26:17 +0100

    2024_01_11_2

--- php/enviar.php
+++ php/enviar.php
@@ -6,6 +6,7 @@ $destino="nicolas.dps@gmail.com";
 $nombre = $_POST["nombre"];
 $email = $_POST["email"];
 $telefono = $_POST["telefono"];
+$mensaje = $_POST["mensaje"];
 
 /* datos que recogemos de su cliente */
 /* ip */
@@ -16,14 +17,15 @@ $ip = $_SERVER['REMOTE_ADDR'];
 $datetime = date("Y-m-d H:i:s");
 
 /* mensaje concatenado para enviar por correo */
-$contenido = "Fecha de envio:".$datetime."\nIP: ".$ip."\nombre: ".$nombre."\nCorreo".$correo."\ntelefono: ".$telefono."\nmensaje: ".$mensaje;
+$contenido = "Fecha de envio: ".$datetime."\nIP: ".$ip."\nNombre: ".$nombre."\nCorreo".$email."\nTelefono: ".$telefono."\nMensaje: ".$mensaje;
 
-$cabecera = "From: info@webda.eus" . "\r\n" . 'Reply-To: info@webda.eus' . "\r\n" ;
+$cabecera = 'From: info@webda.eus'."\r\n".'Reply-To: info@webda.eus'."\r\n" ;
+'X-Mailer PHP/'.phpversion();
 
 /* enviar  correo de confirmacion */
 mail($destino,"consulta de la web",$contenido,$cabecera); /* correo que recibo yo */
 
-mail($correo,"Hemos recibido tu consulta",$contenido,$cabecera); /* correo que recibe el cliente */
+mail($email,"Hemos recibido tu consulta",$contenido,$cabecera); /* correo que recibe el cliente */
 
 /* redirgir  a gracias.html y salir de aqui*/
 header("location:../index.html?enviado=correo enviado!");
